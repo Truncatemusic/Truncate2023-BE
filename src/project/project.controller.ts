b776a18055392234abadfc1120d8437a46aa75ad
Cbanowski <officialeta09@gmail.com>
2023-08-29 19:59:24 +0200

    added createProject api

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post, Req} from '@nestjs/common';
 import {ProjectService} from "./project.service";
+import {AuthService} from "../auth/auth.service";
 
 @Controller('project')
 export class ProjectController {
-    constructor(private service: ProjectService) {}
+    constructor(
+        private readonly service: ProjectService,
+        private readonly authService: AuthService
+    ) {}
+
+    @Post('create')
+    async create(@Req() request: Request, @Body() body: {name: string}) {
+        if (!await this.authService.validateSession(request))
+            return AuthService.INVALID_SESSION_RESPONSE
+
+        return await this.service.createProject(
+            (await this.authService.getSession(request)).user_id,
+            body.name
+        );
+    }
 }
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,7 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, Param} from '@nestjs/common';
 import {PrismaClient} from "@prisma/client";
 
 @Injectable()
 export class ProjectService {
     constructor(private readonly prisma: PrismaClient) {}
+
+    async createProject(@Param('userId') userId: number, @Param('name') name: string) {
+        try {
+            const project = await this.prisma.tproject.create({
+                data: {
+                    name: name
+                }
+            });
+
+            await this.prisma.tprojectuser.create({
+                data: {
+                    user_id: userId,
+                    project_id: project.id,
+                }
+            })
+
+            return { success: true }
+        }
+        catch (_) {
+            return { success: false, reason: "UNKNOWN" }
+        }
+    }
 }
