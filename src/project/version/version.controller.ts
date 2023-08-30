0da1e78a8108e9f96659a02b4409c171e633e094
Cbanowski <officialeta09@gmail.com>
2023-08-30 12:33:11 +0200

    added auth to project version controller

--- src/project/version/version.controller.ts
+++ src/project/version/version.controller.ts
@@ -1,12 +1,19 @@
 import {Body, Controller, Post, Req} from '@nestjs/common';
 import {VersionService} from "./version.service";
+import {AuthService} from "../../auth/auth.service";
 
 @Controller('project/version')
 export class VersionController {
-    constructor(private readonly service: VersionService) {}
+    constructor(
+        private readonly service: VersionService,
+        private readonly authService: AuthService
+    ) {}
 
     @Post('create')
     async createVersion(@Req() request: Request, @Body() body: {projectId: number}) {
+        if (!await this.authService.validateSession(request))
+            return AuthService.INVALID_SESSION_RESPONSE
+
         const result = await this.service.addVersion(parseInt(String(body.projectId)))
         return result
             ? { success: true, version: result }
