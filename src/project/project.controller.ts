d7e4756ce91a81c1a0640d791d7a2f3fed09205b
Cbanowski <officialeta09@gmail.com>
2023-10-25 16:06:13 +0200

    KAN-7 fixed project users get api

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -94,7 +94,7 @@ export class ProjectController {
 
     return {
       success: true,
-      users: this.service.getProjectUsersFill(id),
+      users: await this.service.getProjectUsersFill(id),
     };
   }
 }
