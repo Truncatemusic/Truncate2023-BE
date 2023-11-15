daea835939cbc4b63fcc1f1dedce214095aebed1
Cbanowski <officialeta09@gmail.com>
2023-11-15 18:07:07 +0100

    FIX added mail template scss autocompile

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 import Handlebars from 'handlebars';
 import * as process from 'process';
-import { readFileSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
 import { join } from 'path';
+import { exec } from 'child_process';
 
 @Injectable()
 export class MailService {
@@ -11,6 +12,27 @@ export class MailService {
     process.env.CWD || process.cwd(),
     '/template/mail',
   );
+
+  static checkServiceAvailability(): {
+    success: boolean;
+    error?: string;
+    message?: string;
+  } {
+    if (!existsSync(join(this.DIR, 'css/style.css')))
+      return {
+        success: false,
+        error: 'SCSS_NOT_COMPILED',
+        message:
+          'Mail template SCSS is not compiled! compile using: npm run sass:mail',
+      };
+    return { success: true };
+  }
+
+  static compileScss() {
+    return new Promise<void>(async (resolve) => {
+      exec('npm run sass:mail', async () => resolve());
+    });
+  }
 
   constructor(private readonly mailerService: MailerService) {}
 
