8406e0172ec1d5dd8ecb32f7dce33b27673ae053
Clemens Banowski <officialeta09@gmail.com>
2024-10-07 00:38:04 +0200

    TL-28 added user info to user dropdown

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -34,7 +34,7 @@ export class UserController {
     const userId = await this.authService.getUserId(request);
     if (!userId) return AuthService.INVALID_SESSION_RESPONSE;
 
-    const queryId = isNaN(parseInt(id)) ? undefined : parseInt(id);
+    const queryId = isNaN(+id) ? undefined : +id;
 
     if (queryId && queryId !== userId) {
       const isFollowing = await this.service.isUserFollowing(userId, queryId);
